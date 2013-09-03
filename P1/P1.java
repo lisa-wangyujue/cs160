04c7cf402ddc7f19eeb2a89a50eeae7874fe8ef1
Bobby Signor <signor.bobby@gmail.com>
2013-09-02 19:50:27 -0600

    P1 assignment.

---
+++ P1/P1.java
@@ -0,0 +1,16 @@
+// P1 Assignment
+// Author: Bobby (Robert) Signor
+// Date:   8/28/2013
+// Class:  CS160
+// Email:  bobbysig@rams.colostate.edu
+
+class P1 {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("Signor, Robert");
+		System.out.println("Computer Engineering");
+		System.out.println("CS160");
+		System.out.println("9/3/2013");
+		System.out.println("I am taking this class because it is required for a lot of other classes.");
+	}
+}
